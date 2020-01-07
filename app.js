4384313efd9cf30cfb167dc625d86f250dece36a
Patrycja Dworakowska <dworakowska.pa@gmail.com>
2020-01-08 00:09:18 +0100

    Pass proper random values

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let poker = require("./poker");
 
 let randomHand = poker.getRandomHand();
-let pickedCardsValues = ["6", "6", "6", "3", "3"]; //randomHand.map(element => element.value);
-let pickedCardsSuits = ["trefl", "karo", "pik", "trefl", "karo"]; //randomHand.map(element => element.suite);
+let pickedCardsValues = randomHand.map(element => element.value); // ["6", "6", "6", "3", "3"]
+let pickedCardsSuits = randomHand.map(element => element.suite); // ["trefl", "karo", "pik", "trefl", "karo"]
 
+console.log("Wylosowano:");
 console.log(JSON.stringify(pickedCardsValues));
 console.log(JSON.stringify(pickedCardsSuits));
 
@@ -16,8 +17,8 @@ poker.checkForFull(pickedCardsValues, pickedCardsSuits);
 poker.checkForStraight(pickedCardsValues, pickedCardsSuits);
 poker.checkForFlush(pickedCardsValues, pickedCardsSuits);
 
-console.log(JSON.stringify(poker.possiblePokerSets));
+console.log(`Mozliwe uklady: ${JSON.stringify(poker.possiblePokerSets)}`);
 
 let sorted = poker.possiblePokerSets.sort((a, b) => a.order - b.order);
 let theBest = sorted[sorted.length - 1];
-console.log(theBest);
+console.log(`Najlepszy set: ${JSON.stringify(theBest)}`);
